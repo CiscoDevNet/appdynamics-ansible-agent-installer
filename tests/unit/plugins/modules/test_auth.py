9482668ea73a59d7c6e33562cd17849ef2389e9f
Vitaly Z <zhuravlev.vitaly@gmail.com>
2021-09-10 14:00:32 +0300

    fix sanity tests

--- tests/unit/plugins/modules/test_auth.py
+++ tests/unit/plugins/modules/test_auth.py
@@ -7,7 +7,13 @@ import pytest
 
 from ansible_collections.appdynamics.zeroagent.tests.unit.compat import unittest
 from ansible_collections.appdynamics.zeroagent.tests.unit.compat.mock import patch
-from ansible_collections.appdynamics.zeroagent.tests.unit.plugins.module_utils.utils import exit_json, set_module_args, fail_json, AnsibleFailJson, AnsibleExitJson
+from ansible_collections.appdynamics.zeroagent.tests.unit.plugins.module_utils.utils import (
+    exit_json,
+    set_module_args,
+    fail_json,
+    AnsibleFailJson,
+    AnsibleExitJson,
+)
 
 # from ansible.module_utils import basic
 # from ansible.module_utils.common.text.converters import to_bytes
@@ -23,16 +29,16 @@ def test_name_is_a_required_parameter(capfd):
     out, err = capfd.readouterr()
     results = json.loads(out)
 
-    #assert all(txt in results['msg'] for txt in ('state', 'required'))
+    # assert all(txt in results['msg'] for txt in ('state', 'required'))
     assert results['failed'] is True
     assert "missing required arguments" in results['msg']
+
 
 def test_all_provided(capfd):
 
     with pytest.raises(SystemExit):
         set_module_args({
             "login": "SOMELOGIN",
-            "client": "SOMECLIENT",
             "name": "SOMENAME",
             "user_password": "SOMEPASS",
             "url_password": "SOMEOTHERPASS",
@@ -43,4 +49,4 @@ def test_all_provided(capfd):
     out, err = capfd.readouterr()
     results = json.loads(out)
     assert results['failed'] is True
-    assert "required" not in results['msg'], "msg returned: "+results['msg']
+    assert "required" not in results['msg'], "msg returned: " + results['msg']
